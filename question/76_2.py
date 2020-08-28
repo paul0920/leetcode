4a740319ea1b3ac8fd04e0817d5de7d30e6be03e
Paul <39969716+paul0920@users.noreply.github.com>
2020-08-28 15:22:07 -0700

    the latest update

--- question/76_2.py
+++ question/76_2.py
@@ -16,6 +16,7 @@ if t_item > s_item or t_item == 0 or s == "":
     print ""
     exit()
 
+# Count the characters in the t array
 count_map = collections.Counter(t)
 
 start, end = 0, s_item
@@ -29,17 +30,26 @@ for j, c in enumerate(s):
     if count_map[c] >= 0:
         t_item -= 1
 
+    print j, s[j], i, s[i], count_map
+    print j, i, j-i, " ", end, start, end-start
+
     # All chars in t were found
     if t_item == 0:
 
         # Start looking for the beginning index of the window
         while i < j and count_map[s[i]] < 0:
+
+            print i, s[i]
+
             count_map[s[i]] += 1
             i += 1
 
         if j - i < end - start:
             start = i
             end = j
+
+    print ""
+
 
 # Doesn't find all chars. This means there is no matching window in s
 if t_item:
